57db1241fa53fb20061ac88d2f8fbe5c6e8bf73a
Shadman Saif Anonno <anonno77@gmail.com>
2021-02-20 15:00:19 +0600

    Dataset info more progress

--- preprocess.py
+++ preprocess.py
@@ -11,11 +11,18 @@ def get_df_info(df):
     print(len(df['ID'].unique()))
     print("\nThe number of unique case numbers are")
     print(len(df['Case Number'].unique()))
-    print("\nThe number of null values in the column case numbers are")
+    print("The number of null values in the column case numbers are")
     print(df['Case Number'].isna().sum())
     # Since there are no missing values for case number and the number of unique case number
     # is slightly lower than the number of rows,
     # this means from some cases there are multiple entries
+
+    print("\nThe unique IUCR(Illinois Uniform Crime Reporting code) are")
+    print(df['IUCR'].unique())
+    print("The number of unique IUCR")
+    print(len(df['IUCR'].unique()))
+    print("The number of missing values in this column is IUCR")
+    print(df['IUCR'].isna().sum())
 
 
 if __name__ == "__main__":
